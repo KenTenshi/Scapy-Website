82d48f2fe3c4464704345a8cc2d2cb056c201365
KenTenshi <95683081+KenTenshi@users.noreply.github.com>
2023-05-11 14:49:22 +0200

    Keine Funktion Aktuell

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from scapy.all import rdpcap
+from scapy.layers.inet import IP, ICMP
 import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-def analyze_pcap(file_path):
+def analyze_pcap(file_path, packet_type):
     packets = rdpcap(file_path)
+
+    if packet_type == 'icmp':
+        packets = [pkt for pkt in packets if ICMP in pkt]
+
     # Hier können Sie Ihre Analyse-Logik hinzufügen
     return f"Anzahl der Pakete: {len(packets)}"
 
@@ -14,10 +19,11 @@ def analyze_pcap(file_path):
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
+        packet_type = request.form['packet_type']  # Stellen Sie sicher, dass dieser Code korrekt ist
         if file:
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(file_path)
-            result = analyze_pcap(file_path)
+            result = analyze_pcap(file_path, packet_type)
             return render_template('result.html', result=result)
     return render_template('upload.html')
 

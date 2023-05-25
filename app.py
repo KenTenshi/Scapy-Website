c3609834e6c115a1cdb87be1077a134f4c71cd5b
KenTenshi <95683081+KenTenshi@users.noreply.github.com>
2023-05-25 14:48:05 +0200

    Letzte Changes

--- app.py
+++ app.py
@@ -7,18 +7,20 @@ import os
 
 # Flask-Webanwendung initialisieren
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Konfiguration des Upload-Ordners
 
 # Route für die Startseite der Webanwendung
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)
-            analysis_type = request.form.get('analyse_type')  # Holen Sie sich den Analysetyp aus dem Formular
+    if request.method == 'POST':  # Überprüfen, ob eine POST-Anfrage gesendet wurde
+        file = request.files['file']  # Zugriff auf die hochgeladene Datei
+        if file:  # Überprüfen, ob eine Datei hochgeladen wurde
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)  # Erstellen des Dateipfads zum Speichern der Datei
+            file.save(file_path)  # Speichern der hochgeladenen Datei am angegebenen Pfad
+            
+            analysis_type = request.form.get('analyse_type')  # Abrufen des Analysetyps aus dem Formular
+            
+            # Überprüfen des Analysetyps und Aufrufen der entsprechenden Analysefunktion
             if analysis_type == 'top':
                 analysis_results = analyze_pcap(file_path)
                 return render_template('result_standard.html', result=analysis_results)
@@ -37,7 +39,9 @@ def upload_file():
             elif analysis_type == 'dhcp':
                 analysis_results = analyze_dhcp(file_path)
                 return render_template('result_dhcp.html', result=analysis_results)
+    
+    # Falls keine POST-Anfrage, wird die Startseite zum Hochladen der Datei gerendert
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Starten der Flask-Webanwendung im Debug-Modus
